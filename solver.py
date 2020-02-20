64a891eed32f85f85d164c19b5c05a6efe44eff7
DrDeano <ed.dean515@gmail.com>
2020-02-20 20:50:18 +0000

    Working

--- solver.py
+++ solver.py
@@ -1,6 +1,6 @@
 import math
 
-file_name = "a_example"
+file_name = "b_read_on"
 
 num_of_book = 0
 num_of_libraries = 0
@@ -38,12 +38,12 @@ with open(file_name + ".txt", "r") as in_file:
                 library_count += 1
         line_number += 1
 
-print("Number of books: ", num_of_book)
-print("Number of libraries: ", num_of_libraries)
-print("Number of days: ", num_of_days)
+# print("Number of books: ", num_of_book)
+# print("Number of libraries: ", num_of_libraries)
+# print("Number of days: ", num_of_days)
 
-print("Book scores: ", book_scores)
-print("Libraries: ", libraries)
+# print("Book scores: ", book_scores)
+# print("Libraries: ", libraries)
 
 for library in libraries:
     total_score = 0
@@ -54,7 +54,7 @@ for library in libraries:
     total_days = library[1] + math.ceil(library[0] / library[2])
     library.append(total_days)
 
-print("Libraries: ", libraries)
+# print("Libraries: ", libraries)
 
 def get_highest_book_index(lib):
     book_indexs = lib[3]
@@ -68,64 +68,63 @@ def get_highest_book_index(lib):
 
 current_library_being_set_up = -1
 libraries_sending_books = []
-
-with open(file_name + ".out", "w") as solvedit:
-    solvedit.write(str(final_number_libraries) + "\n")
-    for i in final_libraries :
-        sub = final_libraries[i]
-        for j in sub :
-            if j == 0:
-                solvedit.write(str(sub[j]) + " ")
-            elif j == 1:
-                solvedit.write(str(sub[j]) + "\n")
-            else :
-                subsub = sub[j]
-                for k in subsub:
-                    if k == len(subsub) - 1 :
-                        solvedit.write(str(subsub[k]) + "\n")
-                    else :
-                        solvedit.write(str(subsub[k]) + " ")
-
+libraies_that_have_been_set_up = []
 for day in range(num_of_days):
+    print(day)
     if current_library_being_set_up == -1:
-        current_lib = libraries[0]
+        current_lib = [-1,-1,-1,-1,-1]
         current_lib_index = -1
         for index, lib in enumerate(libraries):
-            if lib[4] > current_lib[4]:
-                current_lib = lib
-                current_lib_index = index
+            if index not in libraies_that_have_been_set_up:
+                if lib[4] > current_lib[4]:
+                    current_lib = lib
+                    current_lib_index = index
         current_library_being_set_up = current_lib_index
         final_libraries.append([current_library_being_set_up,0,[]])
         final_number_libraries += 1
     else:
         if libraries[current_library_being_set_up][1] == 1: # Pretend this is 0 mate
             libraries_sending_books.append(current_library_being_set_up)
+            libraies_that_have_been_set_up.append(current_library_being_set_up)
             current_library_being_set_up = -1
-    libraries[current_library_being_set_up][1] -= 1
+        else:
+            libraries[current_library_being_set_up][1] -= 1
+    
     for lib_index in libraries_sending_books:
-        lib = library[lib_index]
-        book_index = get_highest_book_index(lib)
-        for fin_lib in final_libraries:
-            if fin_lib[0] == lib_index:
-                fin_lib[2].append(book_index)
-                fin_lib[1] += 1
-                break
+        lib = libraries[lib_index]
+        for _ in range(lib[2]):
+            book_index = get_highest_book_index(lib)
+            if book_index != -1:
+                for fin_lib in final_libraries:
+                    if fin_lib[0] == lib_index:
+                        fin_lib[2].append(book_index)
+                        fin_lib[1] += 1
+                        break
 
-        for lib in libraries:
-            lib[3].remove(book_index)
+                for lib_2 in libraries:
+                    try:
+                        lib_2[3].remove(book_index)
+                    except:
+                        pass
 
 print("Final library number: ", final_number_libraries)
 print("Final library array: ", final_libraries)
 
-# for day
-#   if library not being setup:
-#       Start library with highest score
-#       add library to final list
-#   else
-#       decriment library that is being set up
-#       if 0: no library set up next day
-#   for each library that has been set up
-#       send highest scoring book that another library hasn't sent
-#       if none exist take library off, is done
-#
-#
+
+with open(file_name + ".out", "w") as solvedit:
+    solvedit.write(str(final_number_libraries) + "\n")
+    for i in final_libraries:
+        #sub = final_libraries[i]
+        sub = i
+        for j in range(len(sub)):
+            if j == 0:
+                solvedit.write(str(sub[j]) + " ")
+            elif j == 1:
+                solvedit.write(str(sub[j]) + "\n")
+            else:
+                subsub = i[2]
+                for k in range(len(subsub)):
+                    if k == len(subsub) - 1:
+                        solvedit.write(str(subsub[k]) + "\n")
+                    else:
+                        solvedit.write(str(subsub[k]) + " ")
